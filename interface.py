134ae050d60a8865e8e8ea423c78f14bf991eef3
khub41 <62697244+khub41@users.noreply.github.com>
2020-05-05 17:25:50 +0200

    update interface

--- interface.py
+++ interface.py
@@ -10,6 +10,9 @@ height_card = 86
 
 blank_card_image = PhotoImage(file="images/blank.png")
 
+def button_action():
+    print("yo")
+
 #PLAYER A
 
 #Player A has a frame
@@ -22,9 +25,13 @@ name_player_a = Label(frame_player_a, text="Hugo", font=("Helvetica", 20), bg='#
 cards_player_a = Frame(frame_player_a)
 
 for i in range(4):
-    card_a = Canvas(cards_player_a, width=width_card, height=height_card, bg='#c3d8eb', bd=0, highlightthickness=0)
-    card_a.create_image(width_card/2, height_card/2, image=blank_card_image)
+    card_a = Button(cards_player_a, width=width_card, 
+                    height=height_card, bg='#c3d8eb',
+                    image=blank_card_image, command=button_action, 
+                    bd=0, highlightthickness=0)
+    # card_a.image(width_card/2, height_card/2, image=blank_card_image)
     card_a.grid(row=0, column=i)
+
 
 name_player_a.pack() #name is at the TOP
 cards_player_a.pack(side=BOTTOM) #cards are under the name
@@ -32,6 +39,7 @@ cards_player_a.pack(side=BOTTOM) #cards are under the name
 #We pack player's A frame at the Top
 frame_player_a.pack()
 #########END INIT DECK FOR A################
+
 
 #PLAYER B
 frame_player_b = Frame(window, bg='#c3d8eb')
@@ -44,7 +52,6 @@ for i in range(4):
     card_b = Canvas(cards_player_b, width=width_card, height=height_card, bg='#c3d8eb', bd=0, highlightthickness=0)
     card_b.create_image(width_card/2, height_card/2, image=blank_card_image)
     card_b.grid(row=0, column=i)
-
 
 name_player_b.pack(side=BOTTOM)
 cards_player_b.pack()
@@ -64,7 +71,5 @@ depot_area = Canvas(playing_zone, width=width_card, height=height_card, bg='#383
 depot_area.grid(row=0, column=1, padx=10, pady=10)
 
 playing_zone.pack(expand=YES)
-
-window.mainloop()
 
 window.mainloop()
