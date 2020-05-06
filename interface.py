01fb6449a3d154da86ad8d51438c7a9a894cd17a
khub41 <62697244+khub41@users.noreply.github.com>
2020-05-06 14:37:06 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,18 +1,10 @@
 from tkinter import *
 from player import *
 from card import *
-window = Tk()
-window.title("Thalom - the game")
-window.config(background='#c3d8eb')
-window.geometry("720x480")
 
-# width_card = 63
-# height_card = 86
-
-# blank_card_image = PhotoImage(file="images/blank.png")
 class PlayerFrame(Player):
 
-    def __init__(self, name, playground):
+    def __init__(self, name, playground, position):
         super().__init__(name)
 
         #creating the frame
@@ -24,23 +16,24 @@ class PlayerFrame(Player):
         #the frame has cards (canvases)
         cards_player_a = Frame(frame_player_a)
         self.initialize_cards()
-        print(len(self.cards))
         width_card = 63
         height_card = 86
         blank_card_image = PhotoImage(file="images/blank.png")
         for i in range(len(self.cards)):
             card_a = Canvas(cards_player_a, width=width_card, 
-                            height=height_card, bg='#c3d8eb', 
-                            bd=0, highlightthickness=0)
+                            height=height_card, bg='#c3d8eb')
             card_a.create_image(width_card/2, height_card/2, image=blank_card_image)
             card_a.grid(row=0, column=i)
 
-
-        name_player_a.pack() #name is at the TOP
-        cards_player_a.pack(side=BOTTOM) #cards are under the name
+        if position == TOP:
+            name_player_a.pack() #name is at the TOP
+            cards_player_a.pack(side=BOTTOM) #cards are under the name
+        if position == BOTTOM:
+            name_player_a.pack(side=BOTTOM)
+            cards_player_a.pack()
         self.frame = frame_player_a
         #We pack player's A frame at the Top
-        frame_player_a.pack()
+        frame_player_a.pack(side=position)
 
 # def init_player_a():
 #     #Player A has a frame
@@ -88,20 +81,20 @@ class PlayerFrame(Player):
 #     window.update()
 # #########END INIT DECK FOR B################
 
-def init_playing_zone():
+def init_playing_zone(window, width_card, height_card):
     #Playing zone in the middle
     playing_zone = Frame(window, bg='#0a4d1d')
 
     deck_image = PhotoImage(file="images/blank.png") #deck image to be created
     deck = Canvas(playing_zone, width=width_card, height=height_card, bg='#0a4d1d', bd=0, highlightthickness=0)
-    deck.create_image(width_card/2, height_card/2, image=blank_card_image)
+    deck.create_image(width_card/2, height_card/2, image=deck_image)
     deck.grid(row=0, column=0, padx=10, pady=10)
 
     depot_area = Canvas(playing_zone, width=width_card, height=height_card, bg='#383838', bd=0, highlightthickness=0)
     depot_area.grid(row=0, column=1, padx=10, pady=10)
 
     playing_zone.pack(expand=YES)
-    window.update()
+    # window.update()
 
 def main():
     window = Tk()
@@ -109,7 +102,11 @@ def main():
     window.config(background='#c3d8eb')
     window.geometry("720x480")
     Card.initialize_deck_from_json()
-    player_a = PlayerFrame("Hugo", window)
+    player_a = PlayerFrame("Hugo", window, TOP)
+    player_b = PlayerFrame("Cami", window, BOTTOM)
+    width_card = 63
+    height_card = 86
+    init_playing_zone(window, width_card, height_card)
     window.mainloop()
 
 main()
