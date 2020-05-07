24276f90d9b2e7ff2e8f00640ffe7a9ec38284ad
khub41 <62697244+khub41@users.noreply.github.com>
2020-05-07 19:11:16 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,6 +2,17 @@ from tkinter import *
 from player import *
 from card import *
 
+class App(Tk):
+
+    def __init__(self):
+        super().__init__(self)
+        window = Tk()
+        window.title("Thalom - the game")
+        window.config(background='#c3d8eb')
+        window.geometry("720x480")
+        window.mainloop()
+
+
 class CardButton(Button):
     def __init__(self, window, player_name, root_card, index):
         super().__init__(window)
@@ -10,11 +21,43 @@ class CardButton(Button):
         self.value = root_card.value
         self.index = index
 
-
     def show_value(self):
         # print("My value is {}".format(self.value))
         self["text"] = str(self.value)
 
+class DeckButton(Button):
+    def __init__(self, window):
+        super().__init__(window)
+        self.command = None
+        self["text"] = "I'm the deck"
+
+    def activate_pick_button(self, player):
+        self["command"] = player.pick
+        self["command"] = None
+
+
+class DefausseButton(Button):
+    """docstring for DefausseButton"""
+    def __init__(self, window):
+        super().__init__(window)
+        self.command = None
+        self["text"] = "throw your card here"
+    
+    def update_display(self):
+        self["text"] = str(Card.rejected_cards[-1].value)
+
+    def activate_steal_button(self, player):
+        self["command"] = player.steal
+        self["command"] = None
+        self.update_display
+
+class IdButton(Button):
+    
+    def __init__(window, player):
+        super().__init__(window)
+        self["text"] = "Select {}".format(player.name)
+
+        
 # class PlayerFrame(Player):
 
 #     def __init__(self, name, playground, position):
@@ -63,21 +106,9 @@ class PlayerFrame(Player):
 
         #the frame has cards (canvases)
         cards_player_a = Frame(frame_player_a)
-        self.initialize_cards()
-        width_card = 63
-        height_card = 86
-        blank_card_image = PhotoImage(file="images/blank.png")
-        i = 0 # ATTENTION AUX INDEX DES CARTES
-        button_cards = []
-        for card in self.cards:
-            card_button = CardButton(cards_player_a, self.name, card, i)
-            card_button["text"] = "I'm a card"
-            # card_button["command"] = card_button.show_value
-            # card_a.create_image(width_card/2, height_card/2, image=blank_card_image)
-            card_button.grid(row=0, column=i)
-            button_cards.append(card_button)
-            i+=1
-            
+
+        self.frame_cards = cards_player_a
+
         if position == TOP:
             name_player_a.pack() #name is at the TOP
             cards_player_a.pack(side=BOTTOM) #cards are under the name
@@ -87,7 +118,7 @@ class PlayerFrame(Player):
         pass_button = Button(frame_player_a, text="Pass", command=self.end_show)
         pass_button.pack(side=RIGHT)
         self.frame = frame_player_a
-        self.button_cards = button_cards
+        self.button_cards = []
         #We pack player's A frame at the Top
         frame_player_a.pack(side=position)
         
@@ -100,16 +131,33 @@ class PlayerFrame(Player):
         self.button_cards[0]["text"] = "I'm a card"
         self.button_cards[1]["text"] = "I'm a card"
 
+    def update_cards(self):
+        self.initialize_cards()
+        width_card = 63
+        height_card = 86
+        # blank_card_image = PhotoImage(file="images/blank.png")
+        i = 0 # ATTENTION AUX INDEX DES CARTES
+        button_cards = []
+        for card in self.cards:
+            card_button = CardButton(self.frame_cards, self.name, card, i)
+            card_button["text"] = "I'm a card"
+            card_button["command"] = card_button.show_value
+            # card_a.create_image(width_card/2, height_card/2, image=blank_card_image)
+            card_button.grid(row=0, column=i)
+            button_cards.append(card_button)
+            i+=1
+        self.button_cards = button_cards
+
 def init_playing_zone(window, width_card, height_card):
     #Playing zone in the middle
     playing_zone = Frame(window, bg='#0a4d1d')
 
     deck_image = PhotoImage(file="images/blank.png") #deck image to be created
-    deck = Canvas(playing_zone, width=width_card, height=height_card, bg='#0a4d1d', bd=0, highlightthickness=0)
-    deck.create_image(width_card/2, height_card/2, image=deck_image)
+    deck = DeckButton(playing_zone)
+    # deck.create_image(width_card/2, height_card/2, image=deck_image)
     deck.grid(row=0, column=0, padx=10, pady=10)
 
-    depot_area = Canvas(playing_zone, width=width_card, height=height_card, bg='#383838', bd=0, highlightthickness=0)
+    depot_area = DefausseButton(playing_zone)
     depot_area.grid(row=0, column=1, padx=10, pady=10)
 
     playing_zone.pack(expand=YES)
@@ -124,6 +172,8 @@ def main():
     Card.initialize_deck_from_json()
     player_a = PlayerFrame("Hugo", window, TOP)
     player_b = PlayerFrame("Cami", window, BOTTOM)
+    player_a.update_cards()
+    player_b.update_cards()
     width_card = 63
     height_card = 86
     init_playing_zone(window, width_card, height_card)
@@ -131,4 +181,5 @@ def main():
     player_b.show_cards_beginning()
     window.mainloop()
 
-main()
+if __name__ == "__main__":
+    main()
